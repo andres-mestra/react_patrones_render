5dba5f551249ac32a69c2ce0814ffaf10ae3c59f
andres mestra <andresmestra11a@gmail.com>
2021-10-08 11:50:37 -0500

    refactor(Composición de componentes): Composición de componentes para compartir  state

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,46 @@
-import React from 'react';
-import { TodoProvider } from '../TodoContext';
+import { useState } from 'react';
 import { AppUI } from './AppUI';
 
-
 function App() {
+  const [state, setstate] = useState('Estado compartido');
+  return (
+    <>
+      <TodoHeader>
+        <TodoCounter />
+        <TodoSearch />
+      </TodoHeader>
+      <TodoList>
+        <TodoItem state={state} />
+      </TodoList>
+    </>
+  );
+}
+
+function TodoHeader({ children }) {
+  return <header>{children}</header>;
+}
+
+function TodoList({ children }) {
+  return <section className="TodoList-container">{children}</section>;
+}
+
+function TodoSearch() {
+  return <p>TodoSearch</p>;
+}
+
+function TodoCounter() {
+  return <p>TodoCounter</p>;
+}
+
+function TodoItem({ state }) {
+  return <p>TodoItem: {state}</p>;
+}
+/* function App() {
   return (
     <TodoProvider>
       <AppUI />
     </TodoProvider>
   );
-}
+} */
 
 export default App;
